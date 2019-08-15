1ce28b3c5ebe8cf22e687574b132592431937343
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-15 17:20:57 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,7 @@ class Book
   def initialize(title)
     @title = title
   end 
-  attr_accessor :title, :author
+  attr_accessor :title, :author, :page_count
 end  
 
 book = Book.new("And Then There Were None")  
